bdfd949aa623e60cf2b5fb8cd3d30bf8fe05f79b
Laguna1 <lagunazoomarket@gmail.com>
2022-02-01 00:55:44 +0200

    Add test - should not allow the admin attribute to be edited via the web

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -18,6 +18,17 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to login_url
   end
 
+  test 'should not allow the admin attribute to be edited via the web' do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {
+      user: { password: 'password',
+              password_confirmation: 'password',
+              admin: true }
+    }
+    assert_not @other_user.reload.admin?
+  end
+
   test 'should redirect edit when not logged in' do
     get edit_user_path(@user)
     assert_not flash.empty?
