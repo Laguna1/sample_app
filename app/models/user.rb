f4119c32dd1e3d6a04d0cdfc9cac3e353fb4d024
Laguna1 <lagunazoomarket@gmail.com>
2022-02-03 23:34:26 +0200

    Add utility methods for following and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   has_many :active_relationships, class_name: 'Relationship',
                                   foreign_key: 'follower_id',
                                   dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
 
   before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
@@ -29,5 +30,20 @@ class User < ApplicationRecord
   # See "Following users" for the full implementation.
   def feed
     Micropost.where('user_id = ?', id)
+  end
+
+  # Follows a user.
+  def follow(other_user)
+    following << other_user unless self == other_user
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
   end
 end
